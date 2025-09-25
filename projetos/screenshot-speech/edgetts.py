08cd0d5bbf6e9c61ecc8fbb20244388bdca84914
GuiAssuno <gui_ab_@hotmail.com>
2025-09-25 14:47:50 -0300

    mudando nome da pasta

---
+++ projetos/screenshot-speech/edgetts.py
@@ -0,0 +1,12 @@
+import edge_tts
+import asyncio
+
+async def reproduzir_voz(texto):
+    voice = "pt-BR-AntonioNeural" 
+    communicate = edge_tts.Communicate(texto, voice)
+    await communicate.save("audio.mp3")
+    import playsound
+    playsound.playsound("audio.mp3")
+
+if __name__ == "__main__":
+    asyncio.run(reproduzir_voz("Olá, meu nome é fulanodetal"))

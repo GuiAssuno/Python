67ce4dc00064be4337f7f3cf79eff1e697c50b38
GuiAssuno <gui_ab_@hotmail.com>
2025-11-09 23:57:58 -0300

    adicionados as variaveis que podem influenciar

--- projetos/estatistica-mega-sena/scripts/analysis.py
+++ projetos/estatistica-mega-sena/scripts/analysis.py
@@ -1,18 +1,39 @@
 import pandas as pd
 
-# Substitua 'zwei.csv' pelo nome do arquivo que você deseja analisar
-dupla = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/zwei.csv'
-dupla_menos = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/zwei-minus.csv'
-trinca = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/drie.csv'
-quatra = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/vier.csv'
-quina = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/fünf.csv'
+ascendente = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/ascendente.csv'
+descendente = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/descendente.csv'
+dia_concurso = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/dia_concurso.csv'
+dois_grupos = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/dois_grupos.csv'
+trinca_mais_sai = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/drie.csv'
+fibonacci = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/fibonacci.csv'
+frequencia_dezena = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/frequencia_dezena.csv'
+quina_mais_sai = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/fünf.csv'
+mair_tempo_sem_sair = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/mair_tempo_sem_sair.csv'
+mdc = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/mdc.csv'
+mmc = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/mmc.csv'
+multiplicacao_total = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/multiplicacao_total.csv'
+numero_do_consurso = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/numero_do_consurso.csv'
+numero_primos = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/numero_primos.csv'
+ordem_crescente = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/ordem_crescente.csv.csv'
+ordem_saida = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/ordem_saida.csv'
+par_impar = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/par_impar.csv'
+quadrante = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/quadrante.csv'
+soma_total = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/soma_total.csv'
+subtracao_total = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/subtracao_total.csv'
+tamanho_intervalo_entre_dezenas = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/tamanho_intervalo_entre_dezenas.csv'
+tempo_atual_sem_sair = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/tempo_atual_sem_sair.csv'
+total_de_possibilidades = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/total_de_possibilidades.csv'
+tres_grupos = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/tres_grupos.csv'
+quatra_mais_sai = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/vier.csv'
+dupla_menos_sai = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/zwei-minus.csv'
+dupla_mais_sai = '/home/lola/VScode/Python/projetos/estatistica-mega-sena/csv/zwei.csv'
 
 try:
     
     # Carrega o arquivo CSV para um DataFrame
-    df = pd.read_csv(dupla)
+    df = pd.read_csv(dupla_mais_sai)
 
-    print(f"--- Análise Exploratória do Arquivo: {dupla} ---")
+    print(f"--- Análise Exploratória do Arquivo: {dupla_mais_sai} ---")
 
     # 1. Visualizar as 5 primeiras linhas do arquivo
     print("\n--- 5 Primeiras Linhas ---")
@@ -27,6 +48,6 @@ try:
     print(f'{df.describe()}')
 
 except FileNotFoundError:
-    print(f"\nErro: O arquivo '{dupla}' não foi encontrado.")
+    print(f"\nErro: O arquivo '{dupla_mais_sai}' não foi encontrado.")
 except Exception as e:
     print(f"\nOcorreu um erro: {e}")
